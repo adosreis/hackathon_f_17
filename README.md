376fb25b126ca94efc0c1b0a28ab1b088268a869
Andrew Dos Reis <andrewdos2@gmail.com>
2017-10-14 19:40:07 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,5 @@
+hackathon fall 2017
+article classification
+nlp
+nltk
+python
---
+++ nltk_test.py
@@ -0,0 +1,5 @@
+from nltk.corpus import movie_reviews
+documents = [(list(movie_reviews.words(fileid)), category)
+			for category in movie_reviews.categories()
+			for fileid in movie_reviews.fileids(category)]
+random.shuffle(documents)
