4ce9f678cc4fa9d988c955dbae4566612c786dd7
Andrew Dos Reis <andrewdos2@gmail.com>
2017-10-15 08:34:09 -0400

    working(?) with external facing methods

--- nltk_test.py
+++ nltk_test.py
@@ -9,6 +9,7 @@ from sklearn.svm import SVC, LinearSVC, NuSVC
 
 from nltk.classify import ClassifierI
 from statistics import mode
+from newspaper import Article
 
 class VoteClassifier(ClassifierI):
 	def __init__(self, *classifiers):
@@ -32,9 +33,23 @@ class VoteClassifier(ClassifierI):
 run_con = 'new'
 
 documents = []
-for catergory in movie_reviews.categories():
-	for fileid in movie_reviews.fileids(catergory):
-		documents.append((list(movie_reviews.words(fileid)),catergory))
+# for catergory in movie_reviews.categories():
+# 	for fileid in movie_reviews.fileids(catergory):
+# 		documents.append((list(movie_reviews.words(fileid)),catergory))
+tech_docs_f = open('techdocs.pickle','rb')
+tech_docs = pickle.load(tech_docs_f)
+tech_docs_f.close()
+
+
+for doc in tech_docs:
+	documents.append((list(doc.split()),'pos'))
+
+nontech_docs_f = open('nontechdocs.pickle','rb')
+non_tech_docs = pickle.load(nontech_docs_f)
+nontech_docs_f.close()
+
+for doc in non_tech_docs:
+	documents.append((list(doc.split()),'neg'))
 
 random.shuffle(documents)
 word_banks = []
@@ -42,57 +57,52 @@ for w in movie_reviews.words():
 	word_banks.append(w.lower())
 word_banks = nltk.FreqDist(word_banks)
 
-w_features = list(word_banks.keys())[:3000]
+w_features = list(word_banks.keys())
 
 def find_features(document):
 	words = set(document)
 	features = {}
 	for w in w_features:
 		features[w] = (w in words)
-
 	return features
 
 feature_set = [(find_features(rev), catergory) for (rev, catergory) in documents]
 
-training = feature_set[:1900]
-testing = feature_set[1900:]
-
-#LogisticRegression, SGDClassifier
-#SVC, LinearSVC, NuSVC
-
+training = feature_set
 
 classifier = nltk.NaiveBayesClassifier.train(training)
 MNB_Classifier = SklearnClassifier(MultinomialNB()).train(training)
-#GNB_Classifier = SklearnClassifier(GaussianNB())
 BNB_Classifier = SklearnClassifier(BernoulliNB()).train(training)
 LR_Classifier = SklearnClassifier(LogisticRegression()).train(training)
 SGD_Classifier = SklearnClassifier(SGDClassifier()).train(training)
-SVC_Classifier = SklearnClassifier(SVC()).train(training)
 LSVC_Classifier = SklearnClassifier(LinearSVC()).train(training)
 NSVC_Classifier = SklearnClassifier(NuSVC()).train(training)
 
 classifier.show_most_informative_features(100)
-#print("Original accuracy %: ", (nltk.classify.accuracy(classifier, testing))*100)
-#print("MNB accuracy %: ", (nltk.classify.accuracy(MNB_Classifier, testing))*100)
-#print("GNB accuracy %: ", (nltk.classify.accuracy(GNB_Classifier, testing))*100)
-#print("BNB accuracy %: ", (nltk.classify.accuracy(BNB_Classifier, testing))*100)
-#print("LR accuracy %: ", (nltk.classify.accuracy(LR_Classifier, testing))*100)
-#print("SGD accuracy %: ", (nltk.classify.accuracy(SGD_Classifier, testing))*100)
-#print("LSVC accuracy %: ", (nltk.classify.accuracy(LSVC_Classifier, testing))*100)
-#print("NSVC accuracy %: ", (nltk.classify.accuracy(NSVC_Classifier, testing))*100)
+voted_classifier = VoteClassifier(classifier,MNB_Classifier,BNB_Classifier,LR_Classifier,SGD_Classifier,LSVC_Classifier,NSVC_Classifier)
 
-if(run_con is 'new'):
-	voted_classifier = VoteClassifier(classifier,MNB_Classifier,BNB_Classifier,LR_Classifier,SGD_Classifier,LSVC_Classifier,NSVC_Classifier)
+def guess(url):
+	a = Article(url = 'https://www.newscientist.com/article/2150282-online-school-wants-to-train-arts-students-in-cybersecurity/', language = 'en')
+	a.download()
+	a.parse()
+	a_features = find_features(a.text)
+	answer = voted_classifier.classify(a_features)
+	return a ,answer, int(voted_classifier.confidence(a_features)*100)
 
-else:
-	voted_classifier_f = open('voted_classifier.pickle','rb')
-	voted_classifier = pickle.load(voted_classifier_f)
-	voted_classifier_f.close()
+def update(a, answer):
+	if answer is 'pos':
+		non_tech_docs.append(a.text)
+	else:
+		tech_docs.append(a.text)
+	tech_docs_f = open('techdocs.pickle','wb')
+	pickle.dump(tech_docs,tech_docs_f)
+	tech_docs_f.close()
+	nontech_docs_f = open('nontechdocs.pickle','wb')
+	pickle.dump(non_tech_docs,nontech_docs_f)
+	tech_docs_f.close()
 
-print("Voted classifier accuracy %: ", (nltk.classify.accuracy(voted_classifier, testing))*100)
-print("Classification:", voted_classifier.classify(testing[0][0]), "Confidence %:", (voted_classifier.confidence(testing[0][0])*100))
 
-if(run_con is 'new'):
-	voted_classifier_f = open('voted_classifier.pickle','wb')
-	pickle.dump(voted_classifier, voted_classifier_f)
-	voted_classifier_f.close()
+t,b,_ = guess('https://www.theverge.com/2017/9/5/16254300/pixelmator-pro-features-launch-date')
+if b is 'neg':
+	print("WRONG!")
+	update(t,b)
--- scantest.py
+++ scantest.py
@@ -1,13 +1,61 @@
-import urllib.request
-from bs4 import BeautifulSoup
+import pickle
+from newspaper import Article
+tech_urls = []
+nontech_urls = []
+# l1 = 0
+# with open('TECHDATA.txt') as f:
+# 	for line in f:
+# 		tech_urls.append(str(line))
+# 		l1+=1
+# 		if 'str' in line:
+# 			break
+# #print(tech_urls)
+# l2 = 0
+# with open('NONTECH.txt') as fil:
+# 	for line in fil:
+# 		nontech_urls.append(str(line))
+# 		l2 += 0
+# 		if 'str' in line:
+# 			break
+#print(nontech_urls)
+with open('TECHDATA.txt', 'r') as myfile:
+    tech_urls=myfile.read().split()
+with open('NONTECH.txt', 'r') as myfile:
+    nontech_urls=myfile.read().split()
+tech_docs = []
+l = len(tech_urls)
 
-page = urllib.request.urlopen('https://www.wired.com/story/have-a-high-tech-halloween-with-your-own-haunted-smart-home/')
-x = (page.read())
+x = 0
+for tech in tech_urls:
+	try:
+		a = Article(url = str(tech), language = 'en')
+		a.download()
+		a.parse()
+		tech_docs.append(a.text)
+	except:
+		print("error")
+		x+=1
+print(x, "websites failed out of ", l)
+non_tech_docs = []
+l = len(nontech_urls)
+x = 0
+for nontech in nontech_urls:
+	try:
+		a = Article(url = str(nontech), language = 'en')
+		a.download()
+		a.parse()
+		non_tech_docs.append(a.text)
+	except:
+		print("error")
+		x+=1
 
-soup = BeautifulSoup(x, "html.parser")
-words = []
-for string in soup.strings:
-	if(len(string) < 100):
-		if not any(x in string for x in '<>/|\}{^*&%\\'):
-				words.append(string)
-print(words)
+
+print(x, "websites failed out of ", l)
+
+tech_file = open('techdocs.pickle','wb')
+pickle.dump(tech_docs, tech_file)
+tech_file.close()
+
+nontech_file = open('nontechdocs.pickle','wb')
+pickle.dump(non_tech_docs, nontech_file)
+nontech_file.close()
